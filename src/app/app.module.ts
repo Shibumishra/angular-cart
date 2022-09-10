5b479dc44c3cf4a30a12e881f0117016c21fc324
Shibumishra <sibumishra487@gmail.com>
2022-09-10 23:33:57 +0530

    form-binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductItemComponent } from './components/product-item/product-item.component';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { ChangeUsernameComponent } from './components/change-username/change-username.component';
+import { SmsAppComponent } from './components/sms-app/sms-app.component';
 
 @NgModule({
   declarations: [
@@ -16,11 +20,14 @@ import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.
     HeaderComponent,
     FooterComponent,
     ProductItemComponent,
-    ShoppingCartComponent
+    ShoppingCartComponent,
+    ChangeUsernameComponent,
+    SmsAppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
